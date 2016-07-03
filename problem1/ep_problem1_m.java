88d8d1c15734d208b5baec57faf4e031646004ae
Spyros Tzakis <sp.tzakis@yahoo.com>
2016-07-03 22:44:36 +0300

    Added a more mathematical solution

---
+++ problem1/ep_problem1_m.java
@@ -0,0 +1,23 @@
+
+public class ep_problem1 {
+	public static void main(String[] args) {
+
+		long sum;
+
+		int n3 = 999 / 3;
+		int n5 = 999 / 5;
+		int n15 = 999 / 15;
+
+		sum = computeS(n3, 3, 3) + 
+				computeS(n5, 5, 5) -
+				computeS(n15, 15, 15);
+
+		System.out.println("Sum = " + sum);
+		System.out.println("Sum = " + computeS(51, 7, 3));
+	}
+
+
+	static long computeS(int n, int a1, int i) {
+		return n * (2* a1 + (n -1) * i) / 2;
+	}
+}
